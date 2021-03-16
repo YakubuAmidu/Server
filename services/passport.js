cdd75fd9fb6ffd39b471091f596efdda2686b77b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-15 18:24:56 -0700

    Initial commit

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googlId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // We already have a existing user ID
+          done(null, existingUser);
+        } else {
+          // We do not have a user record with this ID, make a new record
+          new User({ googlId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
