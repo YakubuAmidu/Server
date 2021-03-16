5ab6a4e424d37c640f6ab10074fa5e585bae1b86
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-16 00:38:22 -0700

    Commiting changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 
@@ -8,6 +10,13 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 34 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
 require('./routes/authRoutes')(app);
 
