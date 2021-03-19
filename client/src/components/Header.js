49e78654492ac92feb194063d119b1b5519f0a8c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-19 10:11:17 -0700

    Commiting changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a href='#'>Logged out</a>
+            <a href='api/logout'>Logout</a>
           </li>
         );
     }
