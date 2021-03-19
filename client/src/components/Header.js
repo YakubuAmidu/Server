1fe4963aa4d64e5d29818e373c374c313b72eea4
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-18 23:47:25 -0700

    Commiting changes

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <a class='left brand-logo'>Emaily</a>
+          <ul className='right'>
+            <li>
+              <a>Log in with google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
