0dc31c91700452f9073249540ae7282c6cdc461b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-21 00:26:44 -0700

    Commiting changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Payment from './Payment';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -16,10 +16,10 @@ class Header extends Component {
         );
       default:
         return [
-          <li>
-            <Payment />
+          <li key='1'>
+            <Payments />
           </li>,
-          <li>
+          <li key='2'>
             <a href='api/logout'>Logout</a>
           </li>,
         ];
