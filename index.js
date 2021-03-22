815ce277efddf43c355ce45ef8938651c4ee107a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-21 23:43:25 -0700

    Commiting changes

--- index.js
+++ index.js
@@ -24,5 +24,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./api/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve as production assets
+  // Like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file
+  // If it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sentFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
