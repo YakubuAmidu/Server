6cd372d465561a86c48904aea96bcc97514d667c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-23 02:49:02 -0700

    Commiting changes

--- services/Mailer.js
+++ services/Mailer.js
@@ -12,12 +12,21 @@ class Mailer extends helper.Mail {
     this.recipients = this.formatAddresses(recipients);
 
     this.addContent(this.body);
+    this.addClickTracking();
   }
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email(email);
     });
+  }
+
+  addClickingTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.clickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
   }
 }
 
