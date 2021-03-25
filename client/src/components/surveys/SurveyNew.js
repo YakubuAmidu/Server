21e31556f6a1ccb47f5e19e8c749c5a1581f31f5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-25 03:22:37 -0700

    Updating SurveyNew

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,20 @@
 // SurveyNew shows surveyForm and surveryFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+    return <SurveyForm />;
+  }
+
   render() {
-    return (
-      <div>
-        <SurveyForm />
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
