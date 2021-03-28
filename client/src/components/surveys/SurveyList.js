bba1a59bcc88561171a47f5d8738af5e4c6566b9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-28 04:04:15 -0700

    Updating changes

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,12 +3,32 @@ import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
 class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map((survey) => {
+      return (
+        <div className='card darken-1' key={survey._id}>
+          <div className='card-content'>
+            <span className='card-title'>{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className='right'>
+              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
-function mapStateToProps() {
+function mapStateToProps({ surveys }) {
   return { surveys };
 }
 
